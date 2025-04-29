79e14dd05fecb75cf555ee841f279d73de926134
Abhishek <Abhishekraut8697@gmail.com>
2025-04-29 22:23:14 +0530

    ready to deploy

--- client/src/components/WaitlistModal.tsx
+++ client/src/components/WaitlistModal.tsx
@@ -22,7 +22,7 @@ const WaitlistModal: React.FC<WaitlistModalProps> = ({ isOpen, onClose }) => {
     setError('');
 
     try {
-      const response = await fetch('playsyncbacked-production.up.railway.app', {
+      const response = await fetch('https://playsyncbacked-production.up.railway.app/api/v1/waitlist/joinwaitlist', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
